5a28aa9ca7831aa4c69c23aca1ffab89d502b1be
Surya pratap Biswal <suryapratap.biswal@magtapp.in>
2023-04-18 23:08:40 +0530

    add addproduct api

--- controller/products-controller.js
+++ controller/products-controller.js
@@ -10,15 +10,20 @@ export const getProducts = async (req, res) => {
 }
 export const addProduct = async (req, res) => {
     try {
-      
+        const { feedback, details, price, quantity, category, title, img, id } = req.body
+        const result = await Product.create({
+            id, img, title, category, price, quantity, details, feedback
+        })
+        res.send(result)
+
     } catch (error) {
-        
+
     }
 }
 export const getProductById = async (req, res) => {
     try {
         const id = req.params.id
-        const product = await Product.findOne({ "id": id })  
+        const product = await Product.findOne({ "id": id })
         res.status(200).json(product)
     } catch (error) {
         res.status(500).json({ message: error.message })
--- model/product.schema.js
+++ model/product.schema.js
@@ -1,29 +1,5 @@
 import mongoose from "mongoose";
 
-// const productSchema = new mongoose.Schema({
-//   id: {
-//     type: String,
-//     required: true,
-//     unique: true,
-//   },
-//   url: String,
-//   detailUrl: String,
-//   title: {
-//     type: Object,
-//     required: true,
-//   },
-//   price: {
-//     type: Object,
-//     required: true,
-//   },
-//   quantity: Number,
-//   description: String,
-//   discount: String,
-//   tagline: String,
-// });
-
-
-
 const productSchema=  new mongoose.Schema({
   id:{
       type:String,
