bb0f8e26cd29bcf6f8381f6965c55ef3d24c3004
yash371 <jashobantabehera208@gmail.com>
2023-04-18 21:29:09 +0530

    Changing in NewProduct Demo Data

--- constants/newproducts.js
+++ constants/newproducts.js
@@ -15,12 +15,19 @@ export const newproducts=[
         category:"Electronics",
         price:{mrp:69900,cost:57999,discount:17},
         quantity:1,
-        details:{ 
+        details:{ offer:["Combo OfferBuy 2 items save 5%; Buy 3 or more save 8%See all productsT&C",
+        "Flipkart Pay Later",
+        "Special PriceGet extra 29% off (price inclusive of cashback/coupon)T&C",
+      "Bank Offer10% off on IDBI Bank Debit and Credit Card Transactions, up to ₹500. On orders of ₹1,500 and aboveT&C",
+    "No Cost EMI on Bajaj Finserv EMI Card on cart value above ₹2999T&c"],
                  highlight:["128 GB ROM",
                  "15.49 cm (6.1 inch) Super Retina XDR Display",
                  "12MP + 12MP | 12MP Front Camera",
                 "A15 Bionic Chip Processor"],
-                 descriptions:[{img:"https://rukminim1.flixcart.com/image/200/200/cms-rpd-img/f911cab85917488bad55094596200ceb_18359bfe18b_image.png?q=90",text:"Display and Design A more vivid OLED display that’s both easier to see in full sunlight and power efficient. With a durable design that’s water and dust-resistant."}],
+                 descriptions:{
+                    text:`iPhone 13. boasts an advanced dual-camera system that allows you to click mesmerising pictures with immaculate clarity. Furthermore, the lightning-fast A15 Bionic chip allows for seamless multitasking, elevating your performance to a new dimension. A big leap in battery life, a durable design, and a bright Super Retina XDR display facilitate boosting your user experience.`,
+                    detail:[{img:"https://rukminim1.flixcart.com/image/200/200/cms-rpd-img/f911cab85917488bad55094596200ceb_18359bfe18b_image.png?q=90",text:"Display and Design A more vivid OLED display that’s both easier to see in full sunlight and power efficient. With a durable design that’s water and dust-resistant."}]
+                },
                  specification:{general:[{title:"In The Box",text:"iPhone, USB-C to Lightning Cable, Documentation"},{title:"Model Number",text:"MLPG3HN/A"},{title:"Model Name",text:"iPhone 13"}]
                                 },
                  moreinfo:{
@@ -34,6 +41,6 @@ export const newproducts=[
         feedback:{
                   ratting:4.7,
                   reviews:[{question:"Can I pass my exam without studying?",answer:"Yes, if you have a good relationship with your teacher... lol 😂"},{question:"Can I use it for cleaning My balls?",answer:"yes and also clean your wallet too"}]
-                 }                    
+                 }                         
     }
 ]
